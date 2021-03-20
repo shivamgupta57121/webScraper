b6de2d3d3ad6add1757578fe7cec0b149098bf63
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-03-20 13:59:20 +0530

    Upto repository links

--- activity/main.js
+++ activity/main.js
@@ -21,6 +21,31 @@ function extractLink(html){
         let link = selTool(linkElem).attr("href");
         let fullLink = "https://github.com" + link;
         console.log(fullLink);
+        processRepoPage(fullLink);
         
     }
+}
+
+function processRepoPage(link){
+    request(link, cb);
+    function cb(err, response, html){
+        if(err){
+            console.log(err);
+        } else {
+            getRepoLinks(html);
+        }
+    }
+}
+
+function getRepoLinks(html){
+    let selTool = cheerio.load(html);
+    let topicName = selTool(".h1-mktg");
+    console.log(topicName.text());
+    let arr = selTool("a.text-bold");
+    for(let i = 0 ; i < 8 ; i++){
+        let link = selTool(arr[i]).attr("href");
+        let fullLink = "https://github.com" + link;
+        console.log(fullLink);
+    }
+    console.log("``````````````````````````````````");
 }
